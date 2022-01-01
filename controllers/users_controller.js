142ca0a89d50e5802b24d975b6b09533cbf15982
monukumar <monuk.ug19.cs@nitp.ac.in>
2022-01-01 18:42:31 +0530

    before creating posts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -48,5 +48,10 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+  return res.redirect('/');        
+}
+
+module.exports.destroySession=function(req,res){
+   req.logout();
+   res.redirect('/');
 }
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8001;
+const port = 8002;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
+const sassMiddleware=require('sass-middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+}));
 
 app.use(express.urlencoded());
 
@@ -18,13 +31,27 @@ app.set('layout extractScripts', true);
 
 
 // use express router
-app.use('/', require('./routes'));
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(session({
+  name:'codeial',
+  secret:'blah-somethoing',
+  saveUninitialized:false,
+  resave:false,
+   cookie:{
+       maxAge:(1000*60*100)
+   }
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/', require('./routes'));
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
