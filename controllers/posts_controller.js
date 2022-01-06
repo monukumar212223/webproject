40267d5a3a4b16843184b8045d92da222df4684e
monukumar <monuk.ug19.cs@nitp.ac.in>
2022-01-06 12:26:03 +0530

    jquery added convertint to ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,14 +2,25 @@ const Post=require('../models/post')
 const Comment=require('../models/comment');
 module.exports.create=async function(req,res){
     try{
-   await Post.create({
+   let post=await Post.create({
         content:req.body.content,
         user:req.user._id
     });
+    if(req.xhr){
+      return res.status(200).json({
+          data:{
+            post:post
+          },
+          message:"spost created"
+      });
+    }
+
+    req.flash('success','post published');
     return res.redirect('back');
 }
  catch{
  console.log('error',err);
+ req.flash('error',err);
  return ;
   }
 }
@@ -21,14 +32,26 @@ module.exports.destroy= async function(req,res){
        if(post.user==req.user.id){
         post.remove();
       await  Comment.deleteMany({post:req.params.id});
-             return res.redirect('back');
+      
+    if(req.xhr){
+      return res.status(200).json({
+        data:{
+          post_id:req.params.id
+        },message:" deleted"
+      });
+    }
+
+
+      req.flash('success','post deleted');
+      return res.redirect('back');
        }
        else{
+         req.flash('error',"you cannot delete");
            return res.redirect('back'); 
        }
   }
   catch(err){
       console.log('error',err);
+      return res.redirect('back');
   }
-   
     }
