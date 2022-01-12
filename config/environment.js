285a14781ba661269be6d49dfec203b18ba54da5
monukumar <monuk.ug19.cs@nitp.ac.in>
2022-01-12 12:59:36 +0530

    gulp deployment error in aign in/signup css

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,14 @@
+const fs=require('fs');
+const rfs =require('rotating-file-stream');
+const path=require('path');
+
+const logDirectory=path.join(__dirname,'../production_logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream('access.log',{
+    interval:'1d',
+    path: logDirectory
+});
 
 const development={
     name:"development",
@@ -17,11 +28,37 @@ const development={
     google_clientID:"69431482961-ttfi6dmc103e204qodki5vbfdqkc2r5a.apps.googleusercontent.com",
     google_clientSecret:"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",
     google_callbackURL:"http://localhost:8002/users/auth/google/callback",
-    jwt_secret:'codeial'
+    jwt_secret:'codeial',
+    morgan:{
+        node:'combined',
+        options:{stream:accessLogStream}
+    }
 }
 const production={
-    name:"production"
+    name:"production",
+    name:"development",
+    asset_path:'./assets',
+    session_cookie_key:'blahsomething',
+    db:'codeial_production',
+    smtp:{
+        service:'gmail',
+        host:'smtp.gmail.com',
+        post:507,
+        secure:false,
+        auth:{
+            user:'monuchauhan212223',
+            pass:'Monu@2001'
+        }
+    },
+    google_clientID:"69431482961-ttfi6dmc103e204qodki5vbfdqkc2r5a.apps.googleusercontent.com",
+    google_clientSecret:"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",
+    google_callbackURL:"http://codeial.com/users/auth/google/callback",
+    jwt_secret:'codeial',
+    morgan:{
+        node:'combined',
+        options:{stream:accessLogStream}
+    }
 }
 
-module.exports=development;
-
+module.exports=eval(process.env.NODE_ENV)==undefined?development:eval(process.env.NODE_ENV);
+//module.exports=development;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const env=require('./config/environment');
+const logger=require('morgan');
+
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8002;
+require('./config/view-helpers')(app);
+///const port = 8002;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const session=require('express-session');
@@ -21,14 +24,13 @@ const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is on port 5000');
 const path=require('path');
-
-app.use(sassMiddleware({
+/*app.use(sassMiddleware({
     src:'./assets/scss',
     dest:'./assets/css',
     debug:true,
     outputStyle:'extended',
     prefix:'/css'
-}));
+}));*/
 
 app.use(express.urlencoded());
 
@@ -36,6 +38,9 @@ app.use(cookieParser());
 
 app.use(express.static(env.asset_path));
 app.use('/uploads',express.static(__dirname+'/uploads'));
+
+app.use(logger(env.morgan.mode,env.morgan.options));
+
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
@@ -66,10 +71,9 @@ app.use(passport.setAuthenticatedUser);
 app.use(flash());
 app.use(customMware.setFlash);
 app.use('/', require('./routes'));
-app.listen(port, function(err){
+app.listen(process.env.port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
-
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${process.env.port}`);
 });
